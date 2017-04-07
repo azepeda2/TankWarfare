3dce9f0d6ca030e1cb150c4444a4a021fe6c331a
azepeda2 <zepedaalex25@gmail.com>
2017-04-07 01:24:58 -0700

    Added TankMovementComponent which allows controller to throttle tracks forward and back

--- TankWarfare/Source/TankWarfare/Private/Tank.cpp
+++ TankWarfare/Source/TankWarfare/Private/Tank.cpp
@@ -2,6 +2,7 @@
 
 #include "TankWarfare.h"
 #include "TankAimingComponent.h"
+#include "TankMovementComponent.h"
 #include "TankBarrel.h"
 #include "Projectile.h"
 #include "TankTurret.h"
@@ -14,6 +15,7 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("AimingComponent"));
+	//TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("MovementComponent"));
 }
 
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
--- TankWarfare/Source/TankWarfare/Public/TankMovementComponent.h
+++ TankWarfare/Source/TankWarfare/Public/TankMovementComponent.h
@@ -5,15 +5,25 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
- * 
+ *  Responsible for driving the tank tracks
  */
-UCLASS()
+UCLASS( ClassGroup = (Custom), meta = (BlueprintSpawnableComponent) )
 class TANKWARFARE_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
-	
-	
-	
+
+public:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+		void Initialiaze(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+		void IntendMoveForward(float Throw);
+
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 	
 };
